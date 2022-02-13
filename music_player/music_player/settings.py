4ae1f41d55669e0e7d7059b9abcb0aa8499b5db7
Alan-aj <alanj14333@gmail.com>
2022-02-13 19:20:03 +0530

    3. react integration using webpack & babel

--- music_player/music_player/settings.py
+++ music_player/music_player/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
