539201e57fe44be98ad28e1931ff07b7f1bf9bf6
Alan-aj <alanj14333@gmail.com>
2022-02-13 17:06:33 +0530

    api and REST framework

--- music_player/api/views.py
+++ music_player/api/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
-def main(request):
-    return HttpResponse("Hello")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
