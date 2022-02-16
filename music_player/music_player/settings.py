40b58766c118c6af5c1a6d3bbd4352a7492160f8
Alan-aj <87690136+Alan-aj@users.noreply.github.com>
2022-02-16 15:53:44 +0530

    Update settings.py

--- music_player/music_player/settings.py
+++ music_player/music_player/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o_!ovo_1352dk7hh0abbkph!2h9+!y3!eu_#^0fnr*y33qoj8+'
+SECRET_KEY = '-- secret_key --'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
