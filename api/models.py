b166ca59ef3e982b04079ce622447549dc13ae1c
Alan-aj <alanj14333@gmail.com>
2022-03-01 17:02:48 +0530

    added post requests

--- api/models.py
+++ api/models.py
@@ -12,7 +12,7 @@ def generate_unique_code():
 
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
