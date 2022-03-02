eac92a5d52931e1e67b55f98a03c2b6b5e80e90f
Alan-aj <alanj14333@gmail.com>
2022-03-02 23:02:06 +0530

    connect API endpoints

--- api/views.py
+++ api/views.py
@@ -9,6 +9,21 @@ class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
+class GetRoom(APIView):
+    serializer_class = RoomSerializer
+    lookup_url_kwarg = 'code'
+
+    def get(self, request, format=None):
+        code = request.GET.get(self.lookup_url_kwarg)
+        if code != None:
+            room = Room.objects.filter(code=code)
+            if len(room) > 0:
+                data = RoomSerializer(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({'Room Not Found': 'Invalid Room Code'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'Bad Request': 'Code parameter not found in request'}, status=status.HTTP_400_BAD_REQUEST)
+
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
 
--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -8,8 +8,105 @@ import FormControl from '@material-ui/core/FormControl'
 import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
+
+// export default function CreateRoomPage(props) {
+//     defaultVotes = 2;
+//     const [votesToSkip, setVotesToSkip] = defaultVotes;
+//     const [guestCanPause, setGuestCanPause] = useState(true);
+
+//     const construct =() => {
+//         this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+//         this.handleVotesChange = this.handleVotesChange.bind(this);
+//         this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+//     }
+//     construct();
+
+//     const handleVotesChange = e => {
+//         setVotesToSkip(e.target.value);
+//     }
+
+//     const handleGuestCanPauseChange = e => {
+//         setGuestCanPause(e.target.value);
+//     }
+
+
+//     const handleRoomButtonPressed =() =>{
+//         const requestOptions = {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify({
+//                 votes_to_skip: this.state.votesToSkip,
+//                 guest_can_pause: this.state.guestCanPause
+//             })
+//         };
+//         fetch("/api/create-room", requestOptions)
+//             .then((response) => response.json())
+//             .then((data) => {
+//                 const navigate = useNavigate();
+//                 navigate('/room/' + data.code);
+//             })
+//     }
+
+//     render() {
+//         return (
+//             <Grid container spacing={1}>
+//                 <Grid item xs={12} align="center">
+//                     <Typography component="h4" variant="h4">
+//                         Create a Room
+//                     </Typography>
+//                 </Grid>
+//                 <Grid item xs={12} align="center">
+//                     <FormControl component="fieldset">
+//                         <FormHelperText>
+//                             <div align="center">
+//                                 Guest Control of Playback State
+//                             </div>
+//                         </FormHelperText>
+//                         <RadioGroup row defaultValue="true" onChange={this.handleGuestCanPauseChange}>
+//                             <FormControlLabel 
+//                                 value="true" 
+//                                 control={<Radio color="primary" />}
+//                                 label="Play/Pause"
+//                                 labelPlacement="bottom"
+//                             />
+//                             <FormControlLabel 
+//                                 value="false" 
+//                                 control={<Radio color="secondary" />}
+//                                 label="No Control"
+//                                 labelPlacement="bottom"
+//                             />
+//                         </RadioGroup>
+//                     </FormControl>
+//                 </Grid>
+//                 <Grid item xs={12} align="center">
+//                     <FormControl>
+//                         <TextField
+//                             required={true}
+//                             type="number"
+//                             onChange={this.handleVotesChange}
+//                             defaultValue={this.defaultValue}
+//                             inputProps={{
+//                                 min: 1,
+//                                 style: { textAlign: "center" },
+//                             }}
+//                         />
+//                         <FormHelperText>
+//                             <div align="center">Votes required to skip song</div>
+//                         </FormHelperText>
+//                     </FormControl>
+//                 </Grid>
+//                 <Grid item xs={12} align="center">
+//                     <Button color="primary" variant="contained" onClick={this.handleRoomButtonPressed}>Create a Room</Button>
+//                 </Grid>
+//                 <Grid item xs={12} align="center">
+//                     <Button color="secondary" variant="contained" to="/" component={Link}>Back</Button>
+//                 </Grid>
+//             </Grid>
+//         );
+//     }
+// }
 
 export default class CreateRoomPage extends Component {
     defaultVotes = 2;
@@ -48,7 +145,7 @@ export default class CreateRoomPage extends Component {
         };
         fetch("/api/create-room", requestOptions)
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.props.history.push('/room/' + data.code));
     }
 
     render() {
