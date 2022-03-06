9e91a975a1373525986e7574beb60d059fd32e60
Alan-aj <alanj14333@gmail.com>
2022-03-06 18:58:17 +0530

    file changed: Room, CreateRoomPage, HomePage | (react-router-dom version downgraded to 5.2.0 from 6.2.1)

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,24 +1,32 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import JoinRoomPage from "./JoinRoomPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
-import { BrowserRouter as Router,Routes,Route,Link,Redirect} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <Router>
-                <Routes>
-                    <Route exact path='/' element={<p>This is home page</p>} />
-                    <Route path="/join" element={<JoinRoomPage/>} />
-                    <Route path="/create" element={<CreateRoomPage/>} />
-                    <Route path="/room/:roomCode" element={<Room/>} />
-                </Routes>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p>This is the home page</p>
+          </Route>
+          <Route path="/join" component={JoinRoomPage} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room} />
+        </Switch>
+      </Router>
+    );
+  }
 }
--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,60 +1,37 @@
-import React, {useState} from 'react';
-import {useParams} from 'react-router-dom'
+import React, { Component } from "react";
 
+export default class Room extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votesToSkip: 2,
+      guestCanPause: false,
+      isHost: false,
+    };
+    this.roomCode = this.props.match.params.roomCode;
+    this.getRoomDetails();
+  }
 
-export default function Room(props){
-    const [votesToSkip, setVotesToSkip] = useState(2);
-    const [guestCanPause, setGuestCanPause] = useState(false);
-    const [isHost, setIsHost] = useState(false);
-    const {roomCode} = useParams();
-    const getRoomDetails = () => {
-        fetch("/api/get-room" + "?code=" + roomCode).then((response) =>
-            response.json())
-        .then((data) => {
-            setVotesToSkip(data.votes_to_skip);
-            setGuestCanPause(data.guest_can_pause);
-            setIsHost(data.is_host);
+  getRoomDetails() {
+    fetch("/api/get-room" + "?code=" + this.roomCode)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          votesToSkip: data.votes_to_skip,
+          guestCanPause: data.guest_can_pause,
+          isHost: data.is_host,
         });
-    }
-    getRoomDetails();
-    
+      });
+  }
+
+  render() {
     return (
-        <div>
-            <h3>{roomCode}</h3>
-            <p>Votes: {votesToSkip}</p>
-            <p>Guest can pause: {guestCanPause.toString()}</p>
-            <p>Host: {isHost.toString()}</p>
-        </div>
+      <div>
+        <h3>{this.roomCode}</h3>
+        <p>Votes: {this.state.votesToSkip}</p>
+        <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
+        <p>Host: {this.state.isHost.toString()}</p>
+      </div>
     );
+  }
 }
-
-
-// export default class Room extends Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             votesToSkip: 2,
-//             guestCanPause: false,
-//             isHost: false,
-//         };
-        
-        
-//         // console.log(roomCode);
-//         // this.roomCode = this.props.match.params.roomCode;
-//     }
-
-
-//     render() {
-//         const {roomCode} = useParams();
-        
-//         return (
-            
-//             <div>
-//                 <h3>{roomCode}</h3>
-//                 <p>Votes: {this.state.votesToSkip}</p>
-//                 <p>Guest can pause: {this.state.guestCanPause}</p>
-//                 <p>Host: {this.state.isHost}</p>
-//             </div>
-//         );
-//     }
-// }
